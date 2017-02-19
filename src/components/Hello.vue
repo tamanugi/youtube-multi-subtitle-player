f5f202813cbbdd0beafd076429cfcae808d5daec
tamanugi <tamanugi@gmail.com>
2017-02-19 13:19:44 +0900

    implements dual subtitle

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,33 +1,96 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+      <div id="video-player"></div>
+      <button v-on:click="loadYoutube">play!!</button>
+      <div>{{ current }}</div>
+      <div>{{ subtitle_ja }} </div>
+      <div>{{ subtitle_en }} </div>
   </div>
 </template>
 
 <script>
+import YouTubePlayer from 'youtube-player'
+import http from 'http'
+import {parseString} from 'xml2js'
+
 export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      current: 0,
+      player: {},
+      srt: {},
+      subtitle_ja: '',
+      subtitle_en: ''
+    }
+  },
+  methods: {
+    loadYoutube: function () {
+      this.player = YouTubePlayer('video-player')
+      this.player.loadVideoById('-sGiE10zNQM')
+      this.player.stopVideo()
+      this.player.on('ready', () => { this.update() })
+      this.fetchLangSubtitle('ja')
+      this.fetchLangSubtitle('en')
+    },
+    fetchLangSubtitle: function (lang) {
+      let subtitleUrl = `https://www.youtube.com/api/timedtext?fmt=srv3&lang=${lang}&v=-sGiE10zNQM`
+      console.log(subtitleUrl)
+      http.get(subtitleUrl, (res) => {
+        let body = ''
+        res.setEncoding('utf8')
+
+        res.on('data', (chunk) => {
+          body += chunk
+        })
+
+        res.on('end', (res) => {
+          parseString(body, (err, result) => {
+            if (!err) console.dir(result)
+            this.srt[lang] = result.timedtext.body[0].p
+          })
+        })
+      }).on('error', (e) => {
+        console.log(e.message) // エラー時
+      })
+    },
+    update: function () {
+      let setCurrentTime = () => {
+        if (this.player) {
+          this.player.getCurrentTime().then(time => {
+            time = Math.floor(time * 1000)
+            this.current = time
+
+            let subtitleJa = ''
+            for (let i = 0; i < this.srt['ja'].length; i++) {
+              let sub = this.srt.ja[i]
+              let startTime = Number(sub.$.t)
+              let endTime = startTime + Number(sub.$.d)
+              if (startTime <= time && time <= endTime) {
+                subtitleJa = sub._
+                break
+              }
+            }
+            let subtitleEn = ''
+            for (let i = 0; i < this.srt['en'].length; i++) {
+              let sub = this.srt.en[i]
+              let startTime = Number(sub.$.t)
+              let endTime = startTime + Number(sub.$.d)
+              if (startTime <= time && time <= endTime) {
+                subtitleEn = sub._
+                break
+              }
+            }
+            this.subtitle_ja = subtitleJa
+            this.subtitle_en = subtitleEn
+          })
+        }
+      }
+      setCurrentTime()
+      setTimeout(this.update, 50)
     }
   }
+
 }
 </script>
 
